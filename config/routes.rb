ec244847dc08ff5247027831adc97dc3f5c795ca
Tammy Metz <tammy@tammymetz.com>
2019-02-24 22:39:13 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post'
+
+
+
   root 'posts#index'
+
+
 end
